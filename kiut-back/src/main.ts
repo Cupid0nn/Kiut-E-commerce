c1c31f12e2eeda8f369a56b5351bad822c2db0fb
Cupido0nn <malaucierno@mail.com>
2025-01-11 17:46:04 -0300

    Base de datos conectada

--- kiut-back/src/main.ts
+++ kiut-back/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan'; // Importar Morgan
+import * as cors from 'cors'; // Importar CORS
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Añadir Morgan para registrar solicitudes HTTP
+  app.use(morgan('dev'));
+
+  // Añadir CORS para permitir solicitudes entre dominios
+  app.use(cors());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
